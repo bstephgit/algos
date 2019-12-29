cc6a2d5b6d8c5d50cc31f1b6763cf1da18f835f2
Albert <stephane.samara@free.fr>
2019-12-29 12:51:27 +0100

    tree display function

--- js/test/test_tree.js
+++ js/test/test_tree.js
@@ -12,11 +12,34 @@ describe("Tree - depth ", function() {
 		expect(trees.depth(t)).to.equal(4);
 
 	});
-	
+
 	it("Unbalanced", function() {
 		"use strict";
 		var t = createUnbalancedTree();
 		expect(trees.depth(t)).to.equal(4);
+	});
+
+	it("Non trivial tree", function() {
+		var tree = require('../utils').Tree('string');
+
+		var node = tree.createNode('root__x__y__z__');
+
+		tree.setRoot(node);
+
+		node.setLeft(tree.createNode('child1')).setRight(tree.createNode('child2_2312312'));
+
+		node.getLeft().setLeft(tree.createNode('342356436556')).setRight(tree.createNode('234zdasdad'));
+		node.getLeft().getLeft().setLeft(tree.createNode('1234')).setRight(tree.createNode('43'));
+		node.getLeft().getRight().setLeft(tree.createNode('asdwewqrqwr')).setRight(tree.createNode('3213123'));
+		node.getLeft().getLeft().getRight().setLeft(tree.createNode('1')).setRight(tree.createNode('2'));
+		node.getLeft().getRight().getLeft().setLeft(tree.createNode('1')).setRight(tree.createNode('4'));
+
+		node.getRight().setLeft(tree.createNode('23')).setRight(tree.createNode('23435456456'));
+		node.getRight().getLeft().setLeft(tree.createNode('23')).setRight(tree.createNode('23432499234234'));
+		node.getRight().getLeft().getRight().setLeft(tree.createNode('123')).setRight(tree.createNode('456'));
+		node.getRight().getRight().setLeft(tree.createNode('123')).setRight(tree.createNode('93459345435'));
+
+		expect(trees.depth(tree)).to.equal(5);
 	});
 
 });
--- js/trees.js
+++ js/trees.js
@@ -1,3 +1,181 @@
 var Tree = require('./utils').Tree;
 
+function treedepth(tree) {
+	'use strict';
+	var _depth = function(n) {
+		if (!n)
+			return 0;
+		else {
+			var l_depth = _depth(n.getLeft());
+			var r_depth = _depth(n.getRight());
 
+			//console.log('node val:', n.getVal(),'depths',l_depth,r_depth);
+			if (l_depth > r_depth)
+				return l_depth + 1;
+			else
+				return r_depth + 1;
+		}
+	};
+	return _depth(tree.getRoot());
+
+}
+
+module.exports.depth = treedepth;
+/*
+ex:
+root__x__y__z__                                                            // root layer
+child1                              child2_2312312                         // layer + 1
+342356436556  234zdasdad            23                  23435456456        // layer + 2 : parent can be bigger than sum of children
+1234  43      asdwewqrqwr  3213123  23  23432499234234  123  93459345435   // layer + 3
+      3  4    5  6         7  8     9   10								   // sparse layer
+etc...
+
+*/
+module.exports.printTree = function(tree, writable_stream) {
+
+	'use strict';
+
+	var sibling_space = 2;
+
+	var _toString = function(item) {
+		if (item == null)
+			return '(null)';
+		if (item == undefined)
+			return '(undefined)';
+		if (item.getVal)
+			return _toString(item.getVal());
+		return item.toString();
+	};
+
+	var ndlen = function(item) {
+		return _toString(item).length;
+	};
+
+	var children = function(node) {
+		if (node && !node.isLeaf()) {
+			return [node.getLeft(), node.getRight()];
+		}
+		return [];
+	};
+
+	var nextLayer = function(layer) {
+		if (Array.isArray(layer)) {
+			layer = layer.reduce(function(a, n) {
+				return a.concat(children(n));
+			});
+			return (layer.length > 0 ? layer : null);
+		}
+		//is node
+		return children(layer);
+	};
+
+	//create heap
+	var compute_offsets = function(root, offsets, nodes) {
+
+		var tree_length = offsets.length;
+		var max = new Array(tree_length);
+
+		(function lengths_recurse(node, index) {
+
+			max[index] = ndlen(node);
+			nodes[index] = _toString(node);
+
+			if (node && !node.isLeaf()) {
+
+				var children = lengths_recurse(node.getLeft(), 2 * index + 1) + sibling_space + lengths_recurse(node.getRight(), 2 * index + 2);
+				max[index] = Math.max(children, nodes[index].length);
+			}
+
+			return max[index];
+
+		})(root, 0);
+
+		(function check_lengths(index) {
+			if (max[index] && (2 * index + 2) < tree_length) {
+				if (!max[2 * index + 1]) return;
+
+				var max_children = max[2 * index + 1] + max[2 * index + 2] + sibling_space;
+				if (max[index] > max_children) {
+					max[2 * index + 2] = max[index] - max[2 * index + 1] - sibling_space;
+				}
+
+				check_lengths(2 * index + 1);
+				check_lengths(2 * index + 2);
+			}
+		})(0);
+
+		(function lengths_to_offsets() {
+
+			var layer = 0;
+			var index = 0;
+			while (index < tree_length) {
+
+				var index_max = Math.pow(2, layer + 1) - 1;
+				var offset = 0;
+				while (index < tree_length && index < index_max) {
+					if (max[index]) {
+						offsets[index] = offset;
+
+						offset = max[index] + sibling_space - nodes[index].length;
+						index += 1;
+					} else {
+						if (index % 2 != 1) throw 'index ' + index + ' should be odd';
+						if (index + 1 >= tree_length) throw 'index ' + index + ' should less than tree heap length ' + tree_length;
+						//get sibling parent length
+						var parent_index = Math.floor((index - 1) / 2);
+						var next_forward = 2;
+						while (parent_index >= 0 && !max[parent_index]) {
+							parent_index = Math.floor((parent_index - 1) / 2);
+							next_forward *= 2;
+						}
+						if (parent_index > -1) {
+							offset += max[parent_index] + sibling_space;
+							index += next_forward;
+						}
+					}
+				}
+				layer += 1;
+			}
+
+		})();
+	};
+
+	writable_stream = writable_stream || process.stdout;
+
+	var print = function(str) {
+		writable_stream.write(str);
+	};
+
+	var print_blanks = function(w) {
+		print(' '.repeat(w));
+	};
+
+	if (!tree.getRoot()) {
+		print("(empty)");
+		return;
+	}
+	var offsets = new Array(Math.pow(2, treedepth(tree)) - 1);
+	var nodes = new Array(offsets.length);
+	var i = 0;
+	var nb_max = 1;
+
+	compute_offsets(tree.getRoot(), offsets, nodes);
+
+
+	while (i < offsets.length) {
+
+		if ( i >= nb_max )
+		{
+			print('\n');
+			nb_max += nb_max+1;
+		}
+
+		if (offsets[i])
+			print_blanks(offsets[i]);
+		if (nodes[i])
+			print(nodes[i]);
+
+		i += 1;
+	}
+
+};
